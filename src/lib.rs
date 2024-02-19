e309993c2fe65666213bcaaf6280861acadf4afb
kerat <tarek.marce@free.fr>
2024-02-19 23:32:11 +0100

    arg parsing

--- src/lib.rs
+++ src/lib.rs
@@ -15,17 +15,29 @@ pub struct Config {
 
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        // -l : listen mode
+        let lhost:String;
+        let lport:u16;
+        let rhost:String;
+        let rport:u16;
         if args.len() < 2 {
             return Err("not enough arguments")
         }
+        // -l : listen mode
         if args [1] == "-l" {
             println!("listen mode");
+            lhost = String::from("0.0.0.0");
+            lport = args[2].parse::<u16>().unwrap();
+            rhost = String::from("0.0.0.0");
+            rport = 0;
         }
         else {
             println!("connect mode");
+            lhost = String::from("0.0.0.0");
+            lport = 0;
+            rhost = args[1].clone();
+            rport = args[2].parse::<u16>().unwrap();
         }
-        Ok(Config{lhost: "LHOST".to_string(), lport: 01, rhost: "RHOST".to_string(), rport: 02, proto: NetProtocol::TCP})
+        Ok(Config{lhost: lhost, lport: lport, rhost: rhost, rport: rport, proto: NetProtocol::TCP})
     }
 }
 
@@ -42,5 +54,9 @@ impl Config {
 // run 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("run..");
+    println!("{}", config.rhost);
+    println!("{}", config.rport);
+    println!("{}", config.lhost);
+    println!("{}", config.lport);
     Ok(())
 }
